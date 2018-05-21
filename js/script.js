58304913ddaf3d8089f80459f930c45868543aba
Xeng Xiong <waterfish_21@yahoo.com>
2018-05-21 00:57:59 -0500

    google street view api

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+	var $body = $('body');
+	var $wikiElem = $('#wikipedia-links');
+	var $nytHeaderElem = $('#nytimes-header');
+	var $nytElem = $('#nytimes-articles');
+	var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+	// clear out old data before new request
+	$wikiElem.text("");
+	$nytElem.text("");
+	
+	var streetString = $('#street').val();
+	var cityString = $('#city').val();
+	var address = streetString + ', ' + cityString;
+	
+	$greeting.text('So, you want to live at ' + address + '?');
+	
+	var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=800x400&location=' + address + '';
+	
+	$body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+	
 
-    // load streetview
 
-    // YOUR CODE GOES HERE!
+	
 
-    return false;
+
+	return false;
 };
 
 $('#form-container').submit(loadData);
